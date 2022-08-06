bf38126a0744bc462afb42e0248b82f2276705ac
glakpath10 <gkavitha1700@gmail.com>
2022-08-06 12:30:03 +0530

    println modified

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	
 	public static void main(String args[]){
 		
-		System.out.prinln("Kavitha is my Wife!!!!!!!!!!!");
+		System.out.println("Kavitha is my Wife!!!!!!!!!!!");
 	}
 	
 }
